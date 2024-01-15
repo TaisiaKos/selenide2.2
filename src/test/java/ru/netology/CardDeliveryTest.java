e6846378b90bbaa8147066465f94dd4fd6a6b6dd
TaisiaKos <tasia.pcma.le4@mail.ru>
2024-01-15 20:01:41 +0300

    new test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,36 +1,48 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.*;
+import static java.time.Duration.ofSeconds;
 
 public class CardDeliveryTest {
-    private static String getDate(int days, String pattern) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+
+    public String dateTEST(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-        @Test
-        public void shouldBeCorrect() {
 
-        open("http://localhost:7777/");
+    String planningDate = dateTEST(3);
+    SelenideElement notification = $x("//div[@data-test-id='notification']");
 
-        $("[data-test-id=city] input").setValue("Санкт-Петербург");
-        String planningDate = getDate(3, "dd.MM.yyyy");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id=date] input").setValue(planningDate);
-        $("[data-test-id=name] input").setValue("Иванов Иван Иванович");
-        $("[data-test-id=phone] input").setValue("+79210000000");
-        $("[data-test-id=agreement]").click();
-        $("button.button").click();
-        $("[data-test-id=notification]")
-                .shouldBe(Condition.visible, Duration.ofSeconds(20))
-                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + planningDate));
-        }
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    public void cardDelivery() {
+        $x(".//span[@data-test-id='city']//input").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $x(".//span[@data-test-id='name']//input").setValue("Иванов Иван Иванович");
+        $x(".//span[@data-test-id='phone']//input").setValue("+79210000000");
+        $x(".//label[@data-test-id='agreement']").click();
+        $x(".//span[contains(text(),'Забронировать')]").click();
+        notification.should(visible, ofSeconds(15));
+        notification.$x(".//div[@class='notification__title']").should(text("Успешно!"));
+        notification.$x(".//div[@class='notification__content']").should(text("Встреча успешно забронирована на " + planningDate));
+        notification.$x(".//button").click();
+        notification.should(hidden);
+
+    }
 }
+
